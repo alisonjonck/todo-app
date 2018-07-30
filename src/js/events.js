08e691f5f72484e2b568c981170b1f0148c2f160
Alison <alisonjonck@gmail.com>
2018-07-29 21:18:34 -0300

    WEB-101: Suportar a tecla ENTER no input para adicionar um TODO item. Closes #1

--- src/js/events.js
+++ src/js/events.js
@@ -1,16 +1,26 @@
-import {todos} from './state';
-import {listen} from './lib/events';
-import {addTodo, toggleTodoState} from './actions';
+import { todos } from './state';
+import { listen } from './lib/events';
+import { addTodo, toggleTodoState } from './actions';
+
+const dispatchAddTodo = event => {
+    const todoInput = document.getElementById('todoInput');
+    todos.dispatch(addTodo(todoInput.value));
+    event.stopPropagation();
+};
 
 export function registerEventHandlers() {
     listen('click', '#addTodo', event => {
-        const todoInput = document.getElementById('todoInput');
-        todos.dispatch(addTodo(todoInput.value));
-        event.stopPropagation();
+        dispatchAddTodo(event);
     });
 
     listen('click', '.js_toggle_todo', event => {
         const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
         todos.dispatch(toggleTodoState(id));
+    });
+
+    listen('keyup', '#todoInput', event => {
+        if (event.key === 'Enter') {
+            dispatchAddTodo(event);
+        }
     });
 }
